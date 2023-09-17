c6bbd880609a8ad8576b35b8dd23d4696a5595a4
im.bipo <bipinkhatri.ram@gmail.com>
2023-09-18 01:13:31 +0545

    woking ham nav

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Hotel APP
 Alows viewers to reserve and book rooms
 
-live demo : https://im-bipo.github.io/kreebi-hotel
+live demo : https://im-bipo-kreebi-hotel.netlify.app/
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,22 +4,42 @@ import React from 'react'
 
 import './NavBar.css'
 const NavBar = () => {
+    const handleHam = () =>{
+        console.log("clicked");
+        document.getElementById('NavLists').classList.toggle('open')
+        document.getElementById('ham-menu').classList.toggle('open')
+    }
   return (
     <nav className='nav'>
         <h1 className='Logo'>
             Kreebi
         </h1>
-        <ul className='NavLists'>
-            <li className='list'>Home</li>
-            <li className='list'>Services</li>
-            <li className='list'>Rooms</li>
-            <li className='list'>Blog</li>
+        <ul id='NavLists' className='NavLists'>
+            <li className='list'>
+                <a href="/" onClick={handleHam}>Home</a>
+            </li>
+            <li className='list'>
+                <a href="#about" onClick={handleHam}>about</a>
+            </li>
+            <li className='list'>
+                <a href="#contact" onClick={handleHam}>Contact</a>
+            </li>
+            <li className='list'>
+                <a href="#blog" onClick={handleHam}>blog</a>
+            </li>
         </ul>
+        <div className="en-btn">
         <div className='Contact-btn'>
 
             <button className='btn'>
         Contact
             </button>
+        </div>
+        <div id='ham-menu' className='ham-menu' onClick={handleHam}>
+            <div className='ham-bar ham-bar-1'></div>
+            <div className='ham-bar ham-bar-2'></div>
+            <div className='ham-bar ham-bar-3'></div>
+        </div>
         </div>
     </nav>
   )
