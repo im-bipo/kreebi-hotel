af3ee5d7b85ec8b430644c31b54f4c09a2e859de
im.bipo <bipinkhatri.ram@gmail.com>
2023-09-27 21:13:48 +0545

    booking style update

--- src/Components/Feature/DateBooking.jsx
+++ src/Components/Feature/DateBooking.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 const DateBooking = () => {
     return (
-        <div className='flex justify-center md:justify-end lg:block'>
-            <div className='flex max-w-[26rem] flex-wrap mt-20 bg-gradient-to-b from-primaryDark to-[#101E2F] rounded-2xl lg:max-w-none '>
-                <div className='md:m-auto pl-5 py-5 sm:w-1/2 lg:w-fit'>
-                    <h3 className='uppercase text-gray-300 text-sm mb-5'>Arrivail</h3>
+        <div className='flex justify-center lg:items-center md:justify-end lg:block box-content'>
+            <div className='flex max-w-[26rem] flex-wrap mt-14 bg-gradient-to-b from-primaryDark to-[#101E2F] rounded-2xl lg:max-w-none '>
+                <div className=' w-full  p-5 border-b  border-primary sm:border-r sm:w-1/2 lg:w-1/4 flex flex-col justify-center lg:border-b-0'>
+                    <h3 className='uppercase text-gray-300 text-sm mb-5'>Check IN</h3>
                     <div className='flex items-center  text-gray-200 p-5 rounded-md cursor-pointer hover:bg-[#1a2c43] active:bg-[#1a2c43]'>
                         <h6 className=' w-fit text-5xl mx-2'>18</h6>
                         <div className='flex flex-col'>
@@ -13,7 +13,7 @@ const DateBooking = () => {
                         </div>
                     </div>
                 </div>
-                <div className='md:m-auto p-5 sm:w-1/2 lg:w-fit'>
+                <div className='  w-full p-5 border-b border-primary sm:w-1/2 lg:border-b-0 lg:border-r lg:w-1/4 flex flex-col justify-center'>
                     <h3 className='uppercase text-gray-300 text-sm mb-5'>Check Out</h3>
                     <div className='flex items-center  text-gray-200 p-5 rounded-md cursor-pointer hover:bg-[#1a2c43] active:bg-[#1a2c43]'>
                         <h6 className=' w-fit text-5xl mx-2'>18</h6>
@@ -23,7 +23,7 @@ const DateBooking = () => {
                         </div>
                     </div>
                 </div>
-                <div className=' max-w-sm flex m-auto'>
+                <div className='p-5 m-auto lg:w-1/4 flex justify-center  border-r border-primary'>
                     <div className='w-fit  py-5 text-center '>
                         <h6>Adults</h6>
                         <div className='flex justify-center items-center p-5 space-x-2 text-2xl'>
@@ -41,12 +41,14 @@ const DateBooking = () => {
                         </div>
                     </div>
                 </div>
-                <div className='p-5 m-auto items-center justify-center'>
-                    <button className='bg-active text-primary p-2 px-5 rounded-[25rem] lg:w-[10rem] hover:bg-opacity-90 active:bg-opacity-80 '>
-                        Book Now
-                    </button>
-                </div>
+                <div className=' lg:w-1/4 flex justify-center'>
+                    <div className=' p-5 flex m-auto h-full items-center justify-center'>
+                        <button className=' bg-active text-primary p-2 px-5 rounded-[25rem] lg:w-[10rem] hover:bg-opacity-90 active:bg-opacity-80 '>
+                            Book Now
+                        </button>
+                    </div>
 
+                </div>
             </div>
         </div>
     )
--- src/Components/HeroLander.jsx
+++ src/Components/HeroLander.jsx
@@ -4,23 +4,23 @@ import DateBooking from './Feature/DateBooking'
 const HeroLander = () => {
   return (
     <>
-     <div className=' bg-[url("./assets/images/main-banner.jpg")] bg-cover lg:bg-center  bg-no-repeat pt-20 text-white p-5' >
-        <div className='md:m-20'>
+      <div className=' bg-[url("./assets/images/main-banner.jpg")] bg-cover lg:bg-center  bg-no-repeat pt-20 text-white p-5' >
+        <div className='md:m-20 lg:mb-14'>
 
-        <h1 className='w-[5rem] text-[3rem] font-[200]'>
+          <h1 className='w-[5rem] text-[3rem] font-[200]'>
             Exclusive Boutique Hotel
-        </h1>
-        <p className='font-thin text-[.9rem] my-5'>
+          </h1>
+          <p className='font-thin text-[.9rem] my-5'>
             Luxry Botuque hotel
-        </p>
-        <div className='flex space-x-4'>
+          </p>
+          <div className='flex space-x-4'>
             <div className="border w-10 rounded-full px-4 py-2 ">f</div>
             <div className="border w-10 rounded-full px-4 py-2 ">x</div>
             <div className="border w-10 rounded-full px-4 py-2 ">i</div>
+          </div>
         </div>
-        </div>
-        <DateBooking/>
-     </div>
+        <DateBooking />
+      </div>
     </>
   )
 }
