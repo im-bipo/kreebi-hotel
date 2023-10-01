a1eee6a71590a4599dd36781de38873106db0b7d
im.bipo <bipinkhatri.ram@gmail.com>
2023-10-01 12:38:55 +0545

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import HeroLander from './Components/HeroLander'
 const App = () => {
   return (
     <div>
-      <NavBar/>
-      <HeroLander/>
-     <div className='h-[32rem] bg-blue-300'></div>
-     <div className='h-[32rem] bg-blue-200'>spa</div>
-     <div className='h-[32rem] bg-blue-300'>Restruant and Bar</div>
-     <div className='h-[32rem] bg-blue-200'>Advntages</div>
-     <div className='h-[32rem] bg-blue-300'>Rooms</div>
-     <div className='h-[32rem] bg-blue-200'>meter</div>
-     <div className='h-[32rem] bg-blue-300'>testimonial</div>
-     <div className='h-[32rem] bg-blue-200'>footer</div>
+      <NavBar />
+      <HeroLander />
+      <div className='h-[32rem] bg-blue-300'></div>
+      <div className='h-[32rem] bg-blue-200'>spa</div>
+      <div className='h-[32rem] bg-blue-300'>Restruant and Bar</div>
+      <div className='h-[32rem] bg-blue-200'>Advntages</div>
+      <div className='h-[32rem] bg-blue-300'>Rooms</div>
+      <div className='h-[32rem] bg-blue-200'>meter</div>
+      <div className='h-[32rem] bg-blue-300'>testimonial</div>
+      <div className='h-[32rem] bg-blue-200'>footer</div>
     </div>
   )
 }
